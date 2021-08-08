eae1a1b394c3b16d775552cfeda7675261db9e31
mitty1293 <k.fmichi@gmai.com>
2021-08-09 00:54:09 +0900

    3.2まで実施

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ $ git clone https://github.com/mitty1293/rust_the_book.git
 $ sh ./rust_the_book/setup.sh
 $ docker exec -it rust_the_book /bin/bash
 ```
+## 3.2. データ型
+Rustのデータ型は主に以下。
+* スカラー型
+    * 整数（ex. `i32`, `u64`, ...）
+    * 浮動小数点数（`f32`, `f64`）
+    * 論理値（`true`, `false`）
+    * 文字（`char`）
+        * シングルクォートで表される
+* 複合型
+    * タプル
+    * 配列
--- projects/ch03_variables/src/main.rs
+++ projects/ch03_variables/src/main.rs
@@ -1,3 +1,76 @@
+// 3.1. 変数と可変性
+// fn main() {
+//     let mut x = 5;
+//     println!("The value of x is: {}", x);
+//     x = 6;
+//     println!("The value of x is: {}", x);
+// }
+
+
+// 3.1. 変数と可変性 -> シャドーイング
+// fn main() {
+//     let x = 5;
+    
+//     let x = x + 1;
+
+//     let x = x * 2;
+
+//     println!("The value of x is: {}", x);
+// }
+
+
+// 3.2. データ型 -> 浮動小数点型
+// fn main() {
+//     let x = 2.0; // f64
+
+//     let y: f32 = 3.0; // f32
+// }
+
+
+// 3.2. データ型 -> 数値演算
+// fn main() {
+//     // 足し算
+//     let sum = 5 + 10;
+//     println!("sum = {}", sum);
+//     // 引き算
+//     let difference = 95.5 - 4.3;
+//     println!("difference = {}", difference);
+//     // 掛け算
+//     let product = 4 * 30;
+//     println!("product = {}", product);
+//     // 割り算
+//     let quotient = 56.7 / 32.2;
+//     println!("quotient = {}", quotient);
+//     // 余り
+//     let remainder = 43 % 5;
+//     println!("remainder = {}", remainder);
+// }
+
+
+// 3.2. データ型 -> 論理値型
+// fn main() {
+//     let t = true;
+//     println!("t = {}", t);
+
+//     let f: bool = false;
+//     println!("f = {}", f);
+// }
+
+
+// 3.2. データ型 -> 文字型
+// fn main() {
+//     let c = 'z';
+//     println!("c = {}", c);
+//     let z = 'ℤ';
+//     println!("z = {}", z);
+//     let heart_eyed_cat = '😻';
+//     println!("heart_eyed_cat = {}", heart_eyed_cat);
+// } 
+
+
+// 3.2. データ型 -> タプル型
 fn main() {
-    println!("Hello, world!");
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    
 }
