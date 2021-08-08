efd4a869c0bd4d5c569ca21daa120589324d4afe
mitty1293 <k.fmichi@gmai.com>
2021-08-09 04:41:59 +0900

    fix

--- projects/ch03_functions/src/main.rs
+++ projects/ch03_functions/src/main.rs
@@ -10,11 +10,29 @@
 
 
 // 3.3. 関数 -> 関数の引数
-fn main() {
-    another_finction(5, 6);
-}
+// fn main() {
+//     another_finction(5, 6);
+// }
 
-fn another_finction(x: i32, y: i32) {
-    println!("The value of x is: {}", x);
+// fn another_finction(x: i32, y: i32) {
+//     println!("The value of x is: {}", x);
+//     println!("The value of y is: {}", y);
+// }
+
+
+// 3.3. 関数 -> 関数本体は、文と式を含む
+// 以下はコンパイルできない。let y = 6 という文は値を返さないためxに束縛するものがないため。
+// fn main() {
+//     let x = (let y = 6);
+// }
+
+
+// 3.3. 関数 -> 関数本体は、文と式を含む
+fn main() {
+    let x = 5;
+    let y = {
+        let x = 3;
+        x + 1
+    };
     println!("The value of y is: {}", y);
 }
