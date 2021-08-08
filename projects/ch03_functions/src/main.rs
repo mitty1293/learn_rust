200d86fceabb182b63de6c795b59d333f1b0cdc5
mitty1293 <k.fmichi@gmai.com>
2021-08-09 03:30:02 +0900

    3.3を進行中

--- projects/ch03_functions/src/main.rs
+++ projects/ch03_functions/src/main.rs
@@ -1,9 +1,20 @@
 // 3.3. 関数
+// fn main() {
+//     println!("Hello, world!");
+//     another_finction();
+// }
+
+// fn another_finction() {
+//     println!("Another function.");
+// }
+
+
+// 3.3. 関数 -> 関数の引数
 fn main() {
-    println!("Hello, world!");
-    another_finction();
+    another_finction(5, 6);
 }
 
-fn another_finction() {
-    println!("Another function.");
+fn another_finction(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
