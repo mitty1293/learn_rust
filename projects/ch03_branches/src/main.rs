d908f51800b3db792f2364bd7332b9a29f93284e
mitty1293 <k.fmichi@gmai.com>
2021-08-11 03:04:03 +0900

    3.5.フロー制御 -> let文内でif式を使う まで終わり

--- projects/ch03_branches/src/main.rs
+++ projects/ch03_branches/src/main.rs
@@ -1,3 +1,73 @@
-fn main() {
-    println!("Hello, world!");
-}
+// 3.5. フロー制御 -> if式
+// fn main() {
+//     let number = 7;
+
+//     if number < 5 {
+//         println!("condition was true");
+//     } else {
+//         println!("condition was false");
+//     }
+// }
+
+
+// 3.5. フロー制御 -> if式
+// fn main() {
+//     let number = 3;
+
+//     if number {
+//         println!("number was there");
+//     }
+// }
+
+
+// 3.5. フロー制御 -> if式
+// fn main() {
+//     let number = 3;
+
+//     if number != 0 {
+//         println!("number was something other than zero")
+//     }
+// }
+
+
+// 3.5. フロー制御 -> else ifで複数の条件を扱う
+// fn main() {
+//     let number = 6;
+
+//     if number % 4 == 0 {
+//         println!("number is divisible by 4");
+//     } else if number % 3 == 0 {
+//         println!("number is divisible by 3");
+//     } else if  number % 2 == 0 {
+//         println!("number is divisible by 2");
+//     } else {
+//         println!("number is not divisible by 4, 3, or 2");
+//     }
+// }
+
+
+// 3.5. フロー制御 -> let文内でif式を使う
+// fn main() {
+//     let condition = true;
+//     let number = if condition {
+//         5
+//     } else {
+//         6
+//     };
+
+//     println!("The value of number is: {}", number);
+// }
+
+
+// 3.5. フロー制御 -> let文内でif式を使う
+// fn main() {
+//     let condition = true;
+//     let number = if condition {
+//         5
+//     } else {
+//         "six"
+//     };
+
+//     println!("The value of number is: {}", number);
+// }
+
