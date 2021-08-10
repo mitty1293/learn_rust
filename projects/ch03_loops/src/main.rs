f5502dca82a1186f66ae414f471586a71bb507a9
mitty1293 <k.fmichi@gmai.com>
2021-08-11 07:22:25 +0900

    3. 一般的なプログラミングの概念 の完了

--- projects/ch03_loops/src/main.rs
+++ projects/ch03_loops/src/main.rs
@@ -1,5 +1,49 @@
+// 3.5. フロー制御 -> loopでコードを繰り返す
+// fn main() {
+//     loop {
+//         println!("again!");
+//     }
+// }
+
+
+// 3.5. フロー制御 -> whileで条件付きループ
+// fn main() {
+//     let mut number = 3;
+
+//     while number !=0 {
+//         println!("{}!", number);
+//         number = number - 1;
+//     }
+//     println!("LIFTOFF!!!");
+// }
+
+
+// 3.5. フロー制御 -> forでコレクションを覗き見る
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+//     let mut index = 0;
+
+//     while index < 5 {
+//         println!("the value is: {}", a[index]);
+//         index = index + 1;
+//     }
+// }
+
+
+// 3.5. フロー制御 -> forでコレクションを覗き見る
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+
+//     for element in a.iter() {
+//         println!("the value is: {}", element);
+//     }
+// }
+
+
+// 3.5. フロー制御 -> forでコレクションを覗き見る
 fn main() {
-    loop {
-        println!("again!");
+    for number in (1..4).rev() {
+        println!("{}!", number);
     }
+    println!("LIFTOFF!!!");
 }
