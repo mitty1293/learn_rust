3d191bcd5e60605acdc514fe303be3107ce847dc
mitty1293 <k.fmichi@gmai.com>
2021-08-22 09:39:07 +0900

    実験

--- projects/chapter04/ch04_examples/src/main.rs
+++ projects/chapter04/ch04_examples/src/main.rs
@@ -1,4 +1,4 @@
-// // 4.1. 所有権とは？ -> 所有権と関数
+// 4.1. 所有権とは？ -> 所有権と関数
 // fn main() {
 //     let s = String::from("hello");  // sがスコープに入る
 
@@ -25,23 +25,33 @@
 
 
 // 4.1. 所有権とは？ -> 戻り値とスコープ
+// fn main() {
+//     let s1 = gives_ownership();         // gives_ownershipは、戻り値をs1に
+//                                         // ムーブする
+//     let s2 = String::from("hello");     // s2がスコープに入る
+//     let s3 = takes_and_gives_back(s2);  // s2はtakes_and_gives_backにムーブされ
+//                                         // 戻り値もs3にムーブされる
+// } // ここで、s3はスコープを抜け、ドロップされる。s2もスコープを抜けるが、ムーブされているので、
+//   // 何も起きない。s1もスコープを抜け、ドロップされる。
+
+// fn gives_ownership() -> String {             // gives_ownershipは、戻り値を
+//                                              // 呼び出した関数にムーブする
+//     let some_string = String::from("hello"); // some_stringがスコープに入る
+//     some_string                              // some_stringが返され、呼び出し元関数に
+//                                              // ムーブされる
+// }
+
+// // takes_and_gives_backは、Stringを一つ受け取り、返す。
+// fn takes_and_gives_back(a_string: String) -> String { // a_stringがスコープに入る。
+//     a_string  // a_stringが返され、呼び出し元関数にムーブされる
+// }
+
+
+// 4.2. 参照と借用
 fn main() {
-    let s1 = gives_ownership();         // gives_ownershipは、戻り値をs1に
-                                        // ムーブする
-    let s2 = String::from("hello");     // s2がスコープに入る
-    let s3 = takes_and_gives_back(s2);  // s2はtakes_and_gives_backにムーブされ
-                                        // 戻り値もs3にムーブされる
-} // ここで、s3はスコープを抜け、ドロップされる。s2もスコープを抜けるが、ムーブされているので、
-  // 何も起きない。s1もスコープを抜け、ドロップされる。
-
-fn gives_ownership() -> String {             // gives_ownershipは、戻り値を
-                                             // 呼び出した関数にムーブする
-    let some_string = String::from("hello"); // some_stringがスコープに入る
-    some_string                              // some_stringが返され、呼び出し元関数に
-                                             // ムーブされる
+  let s = String::from("hello");
+  change(&s);
 }
-
-// takes_and_gives_backは、Stringを一つ受け取り、返す。
-fn takes_and_gives_back(a_string: String) -> String { // a_stringがスコープに入る。
-    a_string  // a_stringが返され、呼び出し元関数にムーブされる
+fn change(some_string: &String) {
+  some_string.push_str(", world");
 }
