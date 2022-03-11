a0e2414ad6fc4da3a60fbee28dd1a284161fb335
mitty1293 <k.fmichi@gmail.com>
2022-03-12 01:15:55 +0900

    演習の途中

--- setup_local/setup_local.sh
+++ setup_local/setup_local.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+# コンパイル時にはgccリンカをインストールしておく必要がある
+sudo apt-get update
+sudo apt-get install -y build-essential
+# Rustインストール
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh << EOF
 1
 EOF
+# Rustインストール確認
 rustc --version
 cargo --version
+# vscodeで補間等するために必要な静的検査ツールをインストール
+rustup component add rust-src
+rustup component add rust-analysis
+rustup component add rls
