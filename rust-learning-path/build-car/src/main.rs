252b35bc1611c26d137064a5d61abcac362ce379
mitty1293 <k.fmichi@gmail.com>
2022-03-22 16:33:49 +0900

    演習: 車を作成する関数を記述する 途中

---
+++ rust-learning-path/build-car/src/main.rs
@@ -0,0 +1,16 @@
+// Declare Car struct to describe vehicle with four named fields
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+}
+
+#[derive(PartialEq, Debug)]
+// Declare enum for Car transmission type
+enum Transmission {
+    // todo!("Fix enum definition so code compiles");
+    Manual;
+    SemiAuto;
+    Automatic;
+}
