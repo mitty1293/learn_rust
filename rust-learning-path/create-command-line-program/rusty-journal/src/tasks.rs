e63aa18eeec558e21a5029ae494098d22ff05775
mitty1293 <k.fmichi@gmail.com>
2022-04-15 14:30:49 +0900

    メイン モジュールを完成させる まで完了

--- rust-learning-path/create-command-line-program/rusty-journal/src/tasks.rs
+++ rust-learning-path/create-command-line-program/rusty-journal/src/tasks.rs
@@ -1,9 +1,10 @@
 use std::fs::{File, OpenOptions};
 use std::path::PathBuf;
-use std::io::{{Error, ErrorKind, Result, Seek, SeekFrom};
+use std::io::{Error, ErrorKind, Result, Seek, SeekFrom};
 use chrono::{serde::ts_seconds, DateTime, Local, Utc};
 use serde::Deserialize;
 use serde::Serialize;
+use std::fmt;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
@@ -17,6 +18,13 @@ impl Task {
     pub fn new(text: String) -> Task {
         let created_at: DateTime<Utc> = Utc::now();
         Task { text, created_at }
+    }
+}
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
+        write!(f, "{:<50} [{}]", self.text, created_at)
     }
 }
 
@@ -68,6 +76,26 @@ pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> 
     // Write the modified task list back into the file.
     file.set_len(0)?;
     serde_json::to_writer(file, &tasks)?;
+
+    Ok(())
+}
+
+pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
+    // Open the file.
+    let file = OpenOptions::new().read(true).open(journal_path)?;
+    // Parse the file and collect the tasks.
+    let tasks = collect_tasks(&file)?;
+
+    // Enumerate and display tasks, if any.
+    if tasks.is_empty() {
+        println!("Task list is empty!"); 
+    } else {
+        let mut order: u32 = 1;
+        for task in tasks {
+            println!("{}: {}", order, task);
+            order += 1;
+        }
+    }
 
     Ok(())
 }
