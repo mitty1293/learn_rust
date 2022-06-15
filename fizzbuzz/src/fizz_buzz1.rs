765ee37c1419a3b86b3d63393e2cd7a5edc5af4f
mitty1293 <46413939+mitty1293@users.noreply.github.com>
2022-06-15 13:46:31 +0000

    1の完成

--- fizzbuzz/src/fizz_buzz1.rs
+++ fizzbuzz/src/fizz_buzz1.rs
@@ -1,3 +1,17 @@
 pub fn fizz_buzz1() {
-    
+    let mut x: i16 = 1;
+
+    while x <= 50 {
+        if x % 15 ==0 {
+            println!("FizzBuzz");
+        } else if x % 3 == 0 {
+            println!("Fizz");
+        } else if x % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", x);
+        }
+
+        x += 1;
+    }
 }
