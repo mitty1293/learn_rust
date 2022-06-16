c05ae7c67fe739f6c152deef471c0ef55c48ff5e
mitty1293 <46413939+mitty1293@users.noreply.github.com>
2022-06-16 00:51:01 +0000

    2完了

---
+++ fizzbuzz/src/fizz_buzz2.rs
@@ -0,0 +1,13 @@
+pub fn fizz_buzz2() {
+    for x in 1 .. 51 {
+        if x % 15 == 0 {
+            println!("FizzBuzz");
+        } else if x % 3 == 0 {
+            println!("Fizz");
+        } else if x % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", x);
+        }
+    }
+}
--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,5 +1,7 @@
-mod fizz_buzz1;
+// mod fizz_buzz1;
+mod fizz_buzz2;
 
 fn main() {
-    fizz_buzz1::fizz_buzz1();
+    // fizz_buzz1::fizz_buzz1();
+    fizz_buzz2::fizz_buzz2();
 }
