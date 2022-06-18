14d72a1b58c63765796cc345b8d0b62f7aeee5ec
mitty1293 <46413939+mitty1293@users.noreply.github.com>
2022-06-18 14:12:35 +0000

    5完了

---
+++ fizzbuzz/src/fizz_buzz5.rs
@@ -0,0 +1,10 @@
+pub fn fizz_buzz5() {
+    for x in 1 ..= 100 {
+        match (x % 3, x % 5) {
+            (0, 0) => println!("FizzBuzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            _ => println!("{}", x),
+        }
+    }
+}
--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -1,11 +1,13 @@
 // mod fizz_buzz1;
 // mod fizz_buzz2;
 // mod fizz_buzz3;
-mod fizz_buzz4;
+// mod fizz_buzz4;
+mod fizz_buzz5;
 
 fn main() {
     // fizz_buzz1::fizz_buzz1();
     // fizz_buzz2::fizz_buzz2();
     // fizz_buzz3::fizz_buzz3();
-    fizz_buzz4::fizz_buzz4();
+    // fizz_buzz4::fizz_buzz4();
+    fizz_buzz5::fizz_buzz5();
 }
