816dc8c2fa75514515ca20453e50ebabfedb88ca
mitty1293 <k.fmichi@gmail.com>
2024-01-02 14:42:51 +0900

    chapter1終わり

---
+++ highly_efficient_language_Rust_How_to_write_and_create/src/ch1/prime100.py
@@ -0,0 +1,24 @@
+# pythonで素数を100個生成
+
+
+# 素数判定を行う関数
+def is_prime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+
+# count個の素数を生成する
+def get_primes(count):
+    res = []
+    i = 2
+    while len(res) < count:
+        if is_prime(i):
+            res.append(i)
+        i += 1
+    return res
+
+
+# 100個の素数を表示
+print(get_primes(100))
---
+++ highly_efficient_language_Rust_How_to_write_and_create/src/ch1/prime100.rs
@@ -0,0 +1,35 @@
+// pythonで素数を100個生成
+
+
+// 素数判定を行う関数
+fn is_prime(n: usize) -> bool {
+    for i in 2..n {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    return true;
+}
+
+// 素数を100個求める関数
+fn get_primes(primes: &mut[usize; 100]) {
+    let mut i = 2;
+    let mut count = 0;
+    // countが100に達するまで繰り返す
+    while count < 100 {
+        if is_prime(i) {
+            primes[count] = i;
+            count += 1;
+        }
+        i += 1;
+    }
+}
+
+fn main() {
+    // 初期値0ので要素100個の配列を用意する
+    let mut primes = [0; 100];
+    // 素数を100個求める
+    get_primes(&mut primes);
+    // 結果表示
+    println!("{:?}", primes);
+}
