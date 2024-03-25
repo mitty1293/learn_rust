f57923fc359ddaf123ee64f6069bb8df230455eb
mitty1293 <k.fmichi@gmail.com>
2024-03-25 13:57:11 +0900

    p147まで済

--- highly_efficient_language_Rust_How_to_write_and_create/src/ch2/bmi.py
+++ highly_efficient_language_Rust_How_to_write_and_create/src/ch2/bmi.py
@@ -1,3 +1,23 @@
 # pythonでBMI測定ツール
 # 身長と体重の入力
-height_cm = float
+height_cm = float(input("身長(cm)は？ "))
+weight = float(input("体重(kg)は？ "))
+
+# BMIの計算
+height = height_cm / 100.0
+bmi = weight / (height**2)
+print(f"BMI={bmi:.1f}")
+
+# 判定結果を表示
+if bmi < 18.5:
+    print("低体重")
+elif bmi < 25:
+    print("普通体重")
+elif bmi < 30:
+    print("肥満1度")
+elif bmi < 35:
+    print("肥満2度")
+elif bmi < 40:
+    print("肥満3度")
+else:
+    print("肥満4度")
