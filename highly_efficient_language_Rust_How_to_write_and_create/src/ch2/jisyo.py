3fe9b4c1aba72c7830852a3d24d4564c9946ac90
mitty1293 <k.fmichi@gmail.com>
2024-04-03 13:46:17 +0900

    P179終わり

---
+++ highly_efficient_language_Rust_How_to_write_and_create/src/ch2/jisyo.py
@@ -0,0 +1,20 @@
+# pythonで英和辞書
+import sys
+
+dicfile = "ejdict-hand-utf8.txt"
+
+# 引数をチェック
+if len(sys.argv) < 2:
+    print("[USAGE] jisyo.py word")
+    quit()
+# 指定された単語
+word = sys.argv[1]
+
+# 辞書データを１行づつ調べる
+with open(dicfile, "rt", encoding="utf-8") as fp:
+    while True:
+        line = fp.readline()
+        if not line:
+            break
+        if word in line:
+            print(line.strip())
