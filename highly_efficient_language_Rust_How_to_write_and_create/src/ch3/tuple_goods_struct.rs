3e9565042b96de1ccaa71ac41e13265ed44a19b8
mitty1293 <k.fmichi@gmail.com>
2024-05-11 21:57:53 +0900

    P232真ん中まで済.

---
+++ highly_efficient_language_Rust_How_to_write_and_create/src/ch3/tuple_goods_struct.rs
@@ -0,0 +1,28 @@
+// Item構造体（タプルを定義）
+struct Item(String, i64);
+fn main() {
+    // タプルを作る
+    let banana = Item("バナナ".to_string(), 300);
+    let apple = Item("リンゴ".to_string(), 200);
+    let mango = Item("マンゴー".to_string(), 500);
+    // Itemをベクターに追加
+    let items = vec![banana, apple, mango];
+    // 合計金額を求める
+    let total = print_and_sum_items(&items);
+    println!("合計{}円です", total);
+}
+
+// タプルを表示する関数
+fn print_tuple(item: &Item) {
+    println!("{}を{}円で購入", item.0, item.1);
+}
+
+// Itemを順に表示し合計金額を求める関数
+fn print_and_sum_items(items: &Vec<Item>) -> i64 {
+    let mut total = 0;
+    for item in items {
+        print_tuple(&item);
+        total += item.1;
+    }
+    total // 合計金額を返す
+}
