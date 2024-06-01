b6832d7ce400e02ee8cbcbdaaa3efd404cf81ee2
mitty1293 <k.fmichi@gmail.com>
2024-06-01 14:07:38 +0900

    P247まで完

---
+++ highly_efficient_language_Rust_How_to_write_and_create/src/ch3/slice_sum.rs
@@ -0,0 +1,18 @@
+// スライスの各要素を加算する関数
+// 引数に配列の参照=スライスを取る
+fn sum_slice(items: &[i64]) -> i64 {
+    let mut total = 0;
+    for i in items {
+        total += i;
+    }
+    total
+}
+
+fn main() {
+    // 配列を初期化
+    let a = [1,2,3,4,5,6,7,8,9,10];
+    println!("a={}", sum_slice(&a[..]));
+    // ベクターを初期化
+    let b = vec![1,2,3,4,5,6,7,8,9,10];
+    println!("b={}", sum_slice(&b[..]));
+}
