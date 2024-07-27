97af92d2c0466b11764289b83d5c9400d1e39190
mitty1293 <k.fmichi@gmail.com>
2024-07-27 22:00:23 +0900

    sec4-04まで完了 次回sec4-05から

---
+++ highly_efficient_language_Rust_How_to_write_and_create/src/ch4/iter_fib.rs
@@ -0,0 +1,34 @@
+// フィボナッチ数列を表すイテレーター
+struct FibIterator {
+    a: usize,
+    b: usize,
+}
+impl FibIterator {
+    fn new() -> Self {
+        FibIterator {a: 1, b: 1}
+    }
+}
+
+// イテレーターを実装
+impl Iterator for FibIterator {
+    type Item = usize;
+    fn next(&mut self) -> Option<Self::Item> {
+        let tmp = self.a;
+        self.a = self.b;
+        self.b += tmp;
+        return Some(self.a);
+    }
+}
+
+fn main() {
+    // forで結果を5個表示
+    let fib_iter = FibIterator::new();
+    for (i, n) in fib_iter.enumerate() {
+        if i >= 10 {break;}
+        print!("{},", n);
+    }
+    println!("");
+    // takeを使う場合
+    let fib_iter = FibIterator::new();
+    fib_iter.take(10).for_each(|f| print!("{},", f));
+}
